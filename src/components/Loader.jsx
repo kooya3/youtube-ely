b590086b9a209e8a9e967d74650ecaf9da1503dc
kooya3 <elyeestatua@gmail.com>
2023-02-12 06:24:52 +0300

    edited Loader.jsx with a loading block

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Box, CircularProgress, Stack } from '@mui/material';
-
+const Loading = () => (
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}
+  >
+    <CircularProgress />
+  </Box>
+)
 const Loader = () =>  (
   <Box minHeight="95vh">
     <Stack direction='row' justifyContent='center' alignItems='center' height='80vh' >
